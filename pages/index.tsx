0a4530962b461b1cb7d496528373255ffeb401e0
Kumas-Galaxy <kumisamuel86@gmail.com>
2025-06-06 03:11:51 -0700

    Add index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to Kumas Digital Contact Form</h1>
+      <p><a href="/contact">Go to Contact Page</a></p>
+    </div>
+  );
+}
